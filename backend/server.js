dc8b32e02b876826120a9cac7a4e49e5f55eb7e6
Your Name <youremail@example.com>
2025-11-14 10:27:20 +0530

    server.js fixed

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,13 @@ import dotenv from "dotenv";
 import path from "path"; 
 import { fileURLToPath } from "url";
 
-// FIX __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Load .env from SAME folder as server.js
 dotenv.config({
   path: path.join(__dirname, ".env"),
 });
 
-// Debugging check (temporary)
-console.log("Loaded GROQ KEY:", process.env.GROQ_API_KEY);
-console.log("Loaded FAL KEY:", process.env.FAL_KEY);
 
 // Import routes AFTER env loads
 import textRoutes from "./routes/textRoutes.js";
@@ -30,7 +25,7 @@ app.use("/api", textRoutes);
 app.use("/api", imageRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Backend Running ✔");
+  res.send("Backend Running ");
 });
 
 const PORT = process.env.PORT || 5000;
